54b3de9492d4b472d135cf2f7a1be4e75290db5d
Dima Kukula <137822052+DmitryKukula@users.noreply.github.com>
2023-12-26 23:22:54 +0300

    Add files via upload

---
+++ OC/Lab5/Client.cpp
@@ -0,0 +1,111 @@
+﻿#include <conio.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct employee
+{
+	int num; // идентификационный номер сотрудника
+	char name[10]; // имя сотрудника
+	double hours; // количество отработанных часов
+};
+
+int main(int argc, char* argv[]) {
+	int count_of_emp = stoi(argv[1]);
+
+	HANDLE hStartEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, L"Process Started");
+	if (hStartEvent == NULL)
+	{
+		cout << "Client. Open event failed. \nInput any char to exit.";
+		cin.get();
+		return GetLastError();
+	}
+
+	//SetEvent(hStartEvent);
+	HANDLE hPipe = CreateFile(L"\\\\.\\pipe\\pipe_name", GENERIC_WRITE | GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+	if (hPipe == INVALID_HANDLE_VALUE)
+	{
+		cout << "Client. Creation of the named pipe failed. The last error code: " << GetLastError() << "\n";
+		cout << "Press anything to finish server: ";
+		getchar();
+		return 1;
+	}
+
+    while (true) {
+        int chosen_option = 0;
+        cout << "Choose option:\n 1. Modify data\n 2. Exit\nChoose: ";
+        cin >> chosen_option;
+
+        if (chosen_option == 2) {
+            break;
+        }
+        else if (chosen_option == 1) {
+            DWORD dwBytesWritten;
+            DWORD dwBytesReaden;
+
+            int ID;
+            cout << "\nInput an ID of employee: ";
+            cin >> ID;
+
+            if (ID > count_of_emp || ID < 1) {
+                cout << "\nInvalid ID\n";
+                continue;
+            }
+
+            int message_to_send = ID * 10 + chosen_option;
+            bool checker = WriteFile(hPipe, &message_to_send, sizeof(message_to_send), &dwBytesWritten, NULL);
+
+            if (!checker) {
+                cout << "Message wasn't sent.\n";
+                continue;
+            }
+
+            employee emp;
+            if (ReadFile(
+                hPipe, // дескриптор канала
+                &emp, // адрес буфера для ввода данных
+                sizeof(employee), // число читаемых байтов
+                &dwBytesReaden, // число прочитанных байтов
+                NULL)) { // передача данных синхронная
+
+                cout << "ID: " << emp.num << "\tName: " << emp.name << "\tHours: " << emp.hours << "\n";
+                
+                cout << "Input new Name: ";
+                cin >> emp.name;
+                cout << "Input new Hours: ";
+                cin >> emp.hours;
+
+                checker = WriteFile(
+                    hPipe, // дескриптор канала
+                    &emp, // адрес буфера для вывода данных
+                    sizeof(employee), // число записываемых байтов
+                    &dwBytesWritten, // число записанных байтов
+                    NULL); // передача данных синхронная
+
+                if (checker) {
+                    cout << "\nMessage was sent.\n";
+                }
+                else {
+                    cout << "\nMessage wasn't sent.\n";
+                }
+
+                cout << "Press any key to confirm the option...\n";
+                getchar();
+                getchar();
+
+                message_to_send = 1;
+                WriteFile(hPipe, &message_to_send, sizeof(message_to_send), &dwBytesWritten, NULL);
+            }
+            else {
+                cout << "Failed to read employee data.\n";
+            }
+        }
+        else {
+            cout << "Invalid option. Please choose 1 or 2.\n";
+        }
+    }
+    SetEvent(hStartEvent);
+	return 0;
+}
---
+++ OC/Lab5/Server.cpp
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <windows.h>
+#include <conio.h>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+HANDLE hStarted;
+
+struct employee
+{
+	int num;
+	char name[10];
+	double hours;
+};
+
+HANDLE hPipe;
+HANDLE hThreads;
+HANDLE* hSemaphore;
+vector <employee> emps;
+int number_of_employees;
+string file_name;
+
+DWORD WINAPI operations(LPVOID pipe) {
+	HANDLE hPipe = (HANDLE)pipe;
+	DWORD dwBytesRead;
+	DWORD dwBytesWrite;
+
+	int message;
+	while (true) {
+		if (!ReadFile(
+			hPipe, // дескриптор канала
+			&message, // адрес буфера для ввода данных
+			sizeof(message), // число читаемых байтов
+			&dwBytesRead, // число прочитанных байтов
+			NULL)) // передача данных синхронная
+		{
+			//cout << "Data reading from the named pipe failed.\n";
+		}
+		else {
+			int ID = message / 10;
+			int chosen_option = message % 10;
+
+			if (chosen_option == 1) {
+				WaitForSingleObject(hSemaphore[ID - 1], INFINITE);
+
+				employee* emp_to_push = new employee();
+
+				emp_to_push->num = emps[ID - 1].num;
+				emp_to_push->hours = emps[ID - 1].hours;
+				strcpy_s(emp_to_push->name, emps[ID - 1].name);
+
+				bool checker = WriteFile(hPipe, emp_to_push, sizeof(employee), &dwBytesWrite, NULL);
+				if (checker) {
+					cout << "Data to modify was sent.\n";
+				}
+				else {
+					cout << "Data to modify wasn't sent.\n";
+				}
+
+				ReadFile(hPipe, emp_to_push, sizeof(employee), &dwBytesWrite, NULL);
+
+				emps[ID - 1].hours = emp_to_push->hours;
+				strcpy_s(emps[ID - 1].name, emp_to_push->name);
+
+				ofstream file;
+				file.open(file_name);
+
+				for (int i = 0; i < number_of_employees; i++)
+					file << emps[i].num << " " << emps[i].name << " " << emps[i].hours << "\n";
+				file.close();
+
+				int msg;
+				ReadFile(hPipe, &msg, sizeof(msg), &dwBytesWrite, NULL);
+				if (msg == 1) {
+					ReleaseSemaphore(hSemaphore[ID - 1], 1, NULL);
+				}
+			}
+		}
+	}
+	DisconnectNamedPipe(hPipe);
+	CloseHandle(hPipe);
+
+}
+
+int main() {
+	cout << "Input file name: ";
+	cin >> file_name;
+
+	cout << "Input number of employees: ";
+	cin >> number_of_employees;
+
+	ofstream file;
+	emps.resize(number_of_employees);
+
+	for (int i = 0; i < number_of_employees; i++)
+	{
+		cout << "\nInput " << i + 1 << " employee id: ";
+		cin >> emps[i].num;
+		cout << "Input employee name: ";
+		string tmp;
+		cin >> emps[i].name;
+		cout << "Input employee hours: ";
+		cin >> emps[i].hours;
+	}
+
+	file.open(file_name);
+	for (int i = 0; i < number_of_employees; i++)
+		file << emps[i].num << " " << emps[i].name << " " << emps[i].hours << "\n";
+	file.close();
+
+	ifstream file_input;
+	file_input.open(file_name);
+
+	cout << "\n";
+	for (int i = 0; i < number_of_employees; i++)
+	{
+		int id;
+		char name[10];
+		double hours;
+		file_input >> id >> name >> hours;
+		cout << "ID: " << id << "\tName: " << name << "\tHours: " << hours << "\n";
+
+	}
+	file_input.close();
+
+	hPipe = CreateNamedPipe(L"\\\\.\\pipe\\pipe_name", PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES, 0, 0, INFINITE, NULL);
+	hSemaphore = new HANDLE[number_of_employees];
+
+	for (int i = 0; i < number_of_employees; i++)
+	{
+		hSemaphore[i] = CreateSemaphore(NULL, 1, 1, L"hSemahpore");
+	}
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+	wstring CommandLine = (L"Client.exe " + to_wstring(number_of_employees));
+	LPWSTR lpszCommandLine = &CommandLine[0];
+
+	if (!CreateProcess(NULL, lpszCommandLine, NULL, NULL, TRUE,
+		CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+	{
+		cout << "Server. The process is not created.\n";
+		cout << "Press any key to finish.\n";
+		getchar();
+		return GetLastError();
+	}
+	hStarted = CreateEvent(NULL, FALSE, FALSE, L"Process Started");
+	CloseHandle(pi.hProcess);
+
+	if (hPipe == INVALID_HANDLE_VALUE) {
+		cout << "Server. Creation of the named pipe failed. The last error code: " << GetLastError() << "\nPress anything to finish server...";
+		getchar();
+		return GetLastError();
+	}
+
+	if (!ConnectNamedPipe(hPipe, (LPOVERLAPPED)NULL)) {
+		cout << "Server. The connection failed. The last error code: " << GetLastError() << "\n";
+		CloseHandle(hPipe);
+		cout << "Press anything to finish the server: ";
+		getchar();
+		return GetLastError();
+	}
+
+	
+	hThreads = CreateThread(NULL, 0, operations, static_cast<LPVOID>(hPipe), 0, NULL);
+
+	WaitForSingleObject(hStarted, INFINITE);
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	WaitForSingleObject(hThreads, INFINITE);
+
+	cout << "\nThe client has finished the work.\n";
+
+	file_input.open(file_name);
+	for (int i = 0; i < number_of_employees; i++)
+	{
+		int id;
+		char name[10];
+		double hours;
+		file_input >> id >> name >> hours;
+		cout << "ID: " << id << "\tName: " << name << "\tHours: " << hours << "\n";
+
+	}
+	file_input.close();
+	cout << "Press anything to exit...\n";
+	getchar();
+	return 0;
+}
